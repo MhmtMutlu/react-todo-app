1209a73b2517bc8d56eba2f537501df38a7bc2d7
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-29 00:16:26 +0300

    Feat: Custom hook and TodoContext was created.

---
+++ src/context/TodoContext.js
@@ -0,0 +1,42 @@
+import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
+
+export const TodoContext = React.createContext();
+
+const TodoContextProvider = ({ children }) => {
+  const todoList = JSON.parse(localStorage.getItem("todos")) || [];
+  const [todos, setTodos] = useLocalStorage("todos", todoList);
+
+  const addNewTodo = (todo) => {
+    setTodos([...todos, todo]);
+  }
+
+  const removeTodo = (id) => {
+    const filteredTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodo);
+  };
+
+  const changeTodoStatus = (id) => {
+    const indexOfTodo = todos.findIndex((todo) => todo.id === id);
+    todos[indexOfTodo].completed = !todos[indexOfTodo].completed;
+    const newTodo = todos.map(todo => todo);
+    setTodos(newTodo);
+  };
+
+  const values = {
+    todos, 
+    addNewTodo, 
+    removeTodo, 
+    changeTodoStatus
+  }
+
+  return (
+    <TodoContext.Provider
+      value={values}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export default TodoContextProvider;
