663943650d0b0e1434cdb90a5107961542b1e610
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-29 00:18:36 +0300

    Feat-UI: Context API was used. index.html and _index.scss were edited.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
+import List from "./components/List/List";
 import Tabs from "./components/Tabs/Tabs";
+import TodoContextProvider from "./context/TodoContext";
 import Layout from "./layout/Layout";
 
 function App() {
+  
+  //localStorage.setItem("todos", JSON.stringify([{id: "1", text: "mASDAS", completed: true}, {id: "2", text: "asdasd", completed: false}]))
   return (
-    <Layout>
-      <Header />
-      <Form />
-      <Tabs />
-    </Layout>
+    <TodoContextProvider>
+      <Layout>
+        <Header />
+        <Form />
+        <Tabs />
+        <List />
+      </Layout>
+    </TodoContextProvider>
   );
 }
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,12 +1,35 @@
-import React from "react";
+import { nanoid } from "nanoid";
+import React, { useContext, useState } from "react";
+import { TodoContext } from "../../context/TodoContext";
 import "./form.scss";
 
 const Form = () => {
+  const { addNewTodo } = useContext(TodoContext);
+  const [todoText, setTodoText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: nanoid(),
+      text: todoText,
+      completed: false,
+    };
+    addNewTodo(newTodo);
+    setTodoText("");
+  };
+
   return (
     <div className="form-wrapper">
-      <form className="form-container">
-        <button type="submit" className="form-button">New</button>
-        <input className="form-input" placeholder="What's next to be done?"/>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <button type="submit" className="form-button">
+          New
+        </button>
+        <input
+          className="form-input"
+          placeholder="What's next to be done?"
+          value={todoText}
+          onChange={(e) => setTodoText(e.target.value)}
+        />
       </form>
     </div>
   );
