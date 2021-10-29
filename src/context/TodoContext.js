06048e04741493bb33c4be643f9988826fd9189f
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-29 18:11:54 +0300

    Feat: filter state, filteredTodos and changeFilter functions were created.

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 export const TodoContext = React.createContext();
@@ -6,6 +6,21 @@ export const TodoContext = React.createContext();
 const TodoContextProvider = ({ children }) => {
   const todoList = JSON.parse(localStorage.getItem("todos")) || [];
   const [todos, setTodos] = useLocalStorage("todos", todoList);
+  const [filter, setFilter] = useState("all");
+
+  const filteredTodos = () => {
+    if (filter === "all") {
+      return todos;
+    } else if (filter === "active") {
+      return todos.filter((todo) => todo.completed === false);
+    } else if (filter === "completed") {
+      return todos.filter((todo) => todo.completed === true);
+    }
+  };
+
+  const changeFilter = (filterStatus) => {
+    setFilter(filterStatus);
+  };
 
   const addNewTodo = (todo) => {
     setTodos([...todos, todo]);
@@ -32,6 +47,9 @@ const TodoContextProvider = ({ children }) => {
 
   const values = {
     todos,
+    filter,
+    filteredTodos,
+    changeFilter,
     addNewTodo,
     removeTodo,
     removeCompletedTodos,
