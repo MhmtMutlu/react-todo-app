081b70c4ed8623e7d968af7453111c25c0121106
MhmtMutlu <49007453+MhmetMutlu@users.noreply.github.com>
2021-10-29 20:38:44 +0300

    Feat: Modal was opened with edit button.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,38 +1,60 @@
 import React, { useContext, useEffect, useState } from "react";
 import { TodoContext } from "../../context/TodoContext";
+import EditModal from "../Modal/EditModal";
 import "./list.scss";
 
 const List = () => {
-  const { removeTodo, changeTodoStatus, filteredTodos } = useContext(TodoContext);
+  const { removeTodo, changeTodoStatus, filteredTodos } =
+    useContext(TodoContext);
   const [todos, setTodos] = useState([]);
+  const [modalVisibility, setModalVisisibility] = useState(false);
+  const [selectedTodoId, setSelectedTodoId] = useState("");
+
   useEffect(() => {
     setTodos(filteredTodos());
   }, [filteredTodos]);
 
+  const handleModalVisibility = (id) => {
+    setModalVisisibility(true);
+    setSelectedTodoId(id);
+  };
+
   return (
-    <div className="list-container">
-      <ul>
-        {todos.length > 0 &&
-          todos.map((todo) => (
-            <li key={todo.id} className={todo.completed ? "done" : ""}>
-              <span onClick={() => changeTodoStatus(todo.id)}>{todo.text}</span>
-              <span>
-                {todo.completed || (
-                  <button className="edit-button">
-                    <i class="far fa-edit fa-2x"></i>
+    <>
+      <div className="list-container">
+        <ul>
+          {todos.length > 0 &&
+            todos.map((todo) => (
+              <li key={todo.id} className={todo.completed ? "done" : ""}>
+                <span onClick={() => changeTodoStatus(todo.id)}>
+                  {todo.text}
+                </span>
+                <span>
+                  {todo.completed || (
+                    <button
+                      className="edit-button"
+                      onClick={() => handleModalVisibility(todo.id)}
+                    >
+                      <i class="far fa-edit fa-1x"></i>
+                    </button>
+                  )}
+                  <button
+                    className="delete-button"
+                    onClick={() => removeTodo(todo.id)}
+                  >
+                    <i class="far fa-trash-alt fa-1x"></i>
                   </button>
-                )}
-                <button
-                  className="delete-button"
-                  onClick={() => removeTodo(todo.id)}
-                >
-                  <i class="far fa-trash-alt fa-2x"></i>
-                </button>
-              </span>
-            </li>
-          ))}
-      </ul>
-    </div>
+                </span>
+              </li>
+            ))}
+        </ul>
+      </div>
+      <EditModal
+        visibility={modalVisibility}
+        setVisibility={setModalVisisibility}
+        selectedTodoId={selectedTodoId}
+      />
+    </>
   );
 };
 
